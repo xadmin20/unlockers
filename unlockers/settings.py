850575658c0e12aec1cac28440b06e11813fe7dc
Nick <xadmin@bk.ru>
2023-08-30 10:57:17 +0400

    подготовка к переносу на сервер

--- unlockers/settings.py
+++ unlockers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok-free.app']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
